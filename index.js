7501c2ea0cbe90d5f9bc75bbb01f77e2715b8ad6
AzaniaBG <azaniazbaker@gmail.com>
2019-11-26 13:16:57 -0800

    created draft of data model and changed options property to an object (it was an array of multiple choice answers before

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 'use strict'
-const QUIZ = {
-        question: {
-        number: 1,
-        question: "",
-        answer: "",
-    }
-    answers: {
-        option: 1,
-        selected: false,
-        correct: false,
-    }
+const QUIZ = [
+        question1 = {
+            question: "How are you (informal)?",
+            number: 1,
+            options: {
+                option1: "¿Como estas?",
+                option2: "¿Cómo estás?",
+                option3: "¿Cómo eres?",
+                option4: "¿Qué cómo?"
+            },
+            answer: 1,//refers to `options` index ("¿Cómo estás?")
+            selected: null,//need to toggle `selected` class
+            feedback: function() {
+                let selected = question1.selected;
+                if(selected === null) {
+                    return "Please select an answer."
+                } else if(selected === true) {
+                    return `Great! "¿Cómo estás?" is correct; "estar" is the correct verb. Note that "esta" without an accent means "this".`
+                } else {
+                    return `Oops! "¿Cómo estás?" is the correct answer.`
+                }
+            }
+        },
+        question2 = {
+            question: "What are you doing (formal)?",
+            number: 2,
+            options: ["¿Qué haga?", "¿Qué haces?", "¿Qué haciendo?", "¿Qué estás haciendo?"],
+            answer: 3,//refers to `options` index ("¿Qué estás haciendo?")
+            selected: null,//need to toggle `selected` class
+            feedback: function() {
+                let selected = question2.selected;
+                if(selected === null) {
+                    return "Please select an answer."
+                } else if(selected === true) {
+                    return `Great! "¿Qué estás haciendo?" is the correct answer. You can also say, “¿Qué haces?”.`
+                } else {
+                    return `Oops! "¿Qué estás haciendo?" is the correct answer.`
+                };
+            }
+        },
 
-}
+
+]
 
 function handleQuiz() {
 
