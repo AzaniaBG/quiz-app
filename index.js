c5af722fb16519701a1c9ab3cffbeb67bc8b7f3e
AzaniaBG <azaniazbaker@gmail.com>
2019-11-26 07:36:44 -0800

    miscellaneous changes including deleting yml file

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+const QUIZ = {
+        question: {
+        number: 1,
+        question: "",
+        answer: "",
+    }
+    answers: {
+        option: 1,
+        selected: false,
+        correct: false,
+    }
+
+}
+
+function handleQuiz() {
+
+
+}
+
+$(handleQuiz);
