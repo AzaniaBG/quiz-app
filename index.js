6dcae978cb898e98df000fa8b4996d422a3b9142
AzaniaBG <azaniazbaker@gmail.com>
2019-12-02 15:35:06 -0800

    test function partially works

--- index.js
+++ index.js
@@ -123,28 +123,50 @@ const QUIZ = [
 ]
 
 //QUESTIONS is an array of all questions from the QUIZ data model
-let number = 2;
+let number = 1;
 let questions = QUIZ.map(item => {        
         return item.question;
     });
 // console.log(questions);
 
 function handleQuiz() {
-
-    function returnQuestionAtI() {
-    console.log(`returnQuestionAtI ran`);
-        for(let i = 0; i < 5; i++) {
-            let question = questions[i];
-            let num = `${number}`;
-            // console.log(`question is: ${question}`);
-            
-            return question;
-        }
-        
-
+    //function returns a question at the specified index (based on the argument passed in);
+    function returnQuestionAtI(index) {
+        return questions[index];
     };
+
+    function test() {
+
+        console.log(`test ran`);
+        let question;
+        for(let i = 0; i <= questions.length; i++) {
+            let num =+ i;
+            console.log(`num is ${number}`);
+ 
+            if(num === 1) {
+                question = returnQuestionAtI(0);      
+            } else if (num === 2) {
+                question = returnQuestionAtI(1);
+            } else if (num === 3) {            
+                question = returnQuestionAtI(2);
+            } else if (num === 4) {
+                question = returnQuestionAtI(3);
+            } else if (num === 5) {
+                question = returnQuestionAtI(4);
+            } else {
+                console.log(`restart quiz`)
+            }
+            num++
+            console.log(`num is now ${num}`);
+        }
+        console.log(`question is ${question}`)
+        return question;
+    }
+    
+test();
 returnQuestionAtI();
-};
+
+ };
                
 $(handleQuiz);
 
