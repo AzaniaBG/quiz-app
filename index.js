051d0fde9c5e9291f0118927696d291c703acb63
AzaniaBG <azaniazbaker@gmail.com>
2019-12-07 08:38:57 -0800

    created renderQuestionSet function, which returns a question set consisting of a question and answer 4 options. each question set is based on the global variable "number"; question set updates when "number" increases

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const QUIZ = [
 
 ]
 
-
+//NUMBER is used to specify the index for question sets and to show the question number
 let number = 0;
 //QUESTIONS returns an array of all questions from the QUIZ data model
 let questions = QUIZ.map(quiz => {        
@@ -77,41 +77,62 @@ let answers = QUIZ.map(quiz =>{
     return quiz.answers; 
 });
 // console.log(`answers is ${answers}`);
-
+//create a question set from global variable QUESTIONS (line 69) and ANSWERS (line 75) 
     function renderQuestionSet() {
-        //create a question set from global variable QUESTIONS (line 69) and ANSWERS (line 75)
-        
+    //create a question set from global variable QUESTIONS (69) and pass in NUMBER (67) as the index  
         function renderOneQuestion(index) {
             let question = questions[index];
-            console.log(`question is ${question}`);
-            let questionElement = `<h4>${question}</h4>`
-            return questionElement;         
+            // console.log(`question is ${question}`);
+            // let questionElement = `<h4>${question}</h4>`
+            return question;         
         }
-
-console.log(`renderOneQuestion returns:`)
-renderOneQuestion(number);
-// generateQuestionElement();
-        function renderAnswerSet(answers) {
-        //each answer is a set of 4 answers:
-                let answer = answers.map((answer) => {
-                // console.log(`answer is: ${answer}`)
-                    return answer;
-                });
-                return answer;
-            }
         
+//create an answer set from global variable ANSWERS (line 75) and pass in NUMBER (line 67) as the index 
         function renderOneAnswerSet(index) {
-            let answerSet = answers[index];
-            console.log(`answerSet is ${answerSet}`);
-             
-            return answerSet;
+                let answerSet = answers[index];
+        // console.log(`answerSet is ${answerSet}`);         
+                return answerSet;
+            }
 
+        function generateQuestionSet(number) {
+            let answer = renderOneAnswerSet(number);
+            console.log(`answer is ${answer}`);
+            let oneQuestion = renderOneQuestion(number);
+
+            let questionSet = `
+            <h4 role="" class="form js-main-screen js-question-set" id="js-question${number}">${oneQuestion}</h4>
+            <input role="" class="radio js-button-index0" type="radio" name="options" value="0" checked id="index0">
+            <label for="index0" class="radio js-button-index0" lang="es"> </label>
+            <button role="button" class="button js-main-screen js-button-index0"> ${answer[0]}</button>
+            <br>
+            <input role="" class="radio js-button-index1" type="radio" name="options" value="1" checked id="index1">
+            <label for="index1" class="radio js-button-index1" lang="es"> </label>
+            <button role="button" class="button js-main-screen js-button-index1"> ${answer[1]}</button>
+            <br>
+            <input role="" class="radio js-button-index2" type="radio" name="options" value="2" checked id="index2">
+            <label for="index2" class="radio js-button-index2" lang="es"> </label>
+            <button role="button" class="button js-main-screen js-button-index2"> ${answer[2]}</button>
+            <br>
+            <input role="" class="radio js-button-index2" type="radio" name="options" value="2" checked id="index2">
+            <label for="index3" class="radio js-button-index3" lang="es"> </label>
+            <button role="button" class="button js-main-screen js-button-index3"> ${answer[3]}</button>
+            `
+            $(".js-button-start").click(function(event) {
+                event.preventDefault();
+            $(".js-question-set").html(questionSet);
+            });
+
+        console.log(`questionSet is ${questionSet}`)
+            return questionSet;
         }
 
-console.log(`renderAnswerSet returns:`);
-    renderAnswerSet(answers);
-console.log(`renderOneAnswerSet returns: `)
-    renderOneAnswerSet(number)
+
+    console.log(`generateQuestionSet returns:`)
+        generateQuestionSet(number);
+    console.log(`renderOneQuestion returns:`)
+        renderOneQuestion(number);
+    console.log(`renderOneAnswerSet returns: `)
+        renderOneAnswerSet(number)
     }
 
 //when START button clicked, START button hides, SUBMIT and NEXT button display
@@ -139,8 +160,7 @@ console.log(`renderOneAnswerSet returns: `)
     //     // }
     // }
 //returnQuestionAtI() returns a question at the specified index, i.e., `NUMBER` 
-    
-    
+      
     function restartQuiz() {
         
     };
@@ -155,6 +175,7 @@ function handleQuiz() {
 
     // showQuestionNumber();
     handleNextButton();
+    //generateQuestionSet();
     // renderOneQuestion();
     // renderAnswerSet();
     renderQuestionSet();
