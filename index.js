fbfa6b48108af7825e44620027d3329fe8f1db47
AzaniaBG <azaniazbaker@gmail.com>
2019-12-08 07:58:44 -0800

    checkQuestionSet prevents `number` from incrementing beyond 5

--- index.js
+++ index.js
@@ -84,9 +84,13 @@ let questionNumber = `<output role="header" class="info js-question-number" valu
 let questionsCorrect = `<output role="header" class="info js-answers-correct">Answers correct 0/0 </output>`;
 // console.log(`answers is ${answers}`);
 //create a question set from global variable QUESTIONS (line 69) and ANSWERS (line 75) 
-    function startQuiz() {
+    function handleQuiz() {
     //create a question set from global variable QUESTIONS (69) and pass in NUMBER (67) as the index  
-        
+        if(number <= 5) {
+            startQuiz();
+        } else {
+            showResultsScreen();
+        }
         
 //create an answer set from global variable ANSWERS (line 75) and pass in NUMBER (line 67) as the index 
 
@@ -122,10 +126,8 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
     //     // }
     // }
 //returnQuestionAtI() returns a question at the specified index, i.e., `NUMBER` 
-    function handleStartButton() {
-        // let questionNumber = `<output role="header" class="info js-question-number" value="num"> Question Number ${number+1} </output>`;
-        // let questionsCorrect = `<output role="header" class="info js-answers-correct">Answers correct 0/0 </output>`;           
-        // let questionSet = generateQuestionSet(number);
+    
+function handleStartButton() {
         $(".js-button-start").click(function(event) {
             event.preventDefault();
             $(".js-start-screen").toggle();
@@ -133,11 +135,9 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
             
             $(".js-question-number").html(`${questionNumber}`);
             $(".js-answers-correct").html(`${questionsCorrect}`);
-            // $("h2").append(`${questionNumber}`);
-            // $(".js-question-number").append(`<output role="header" class="js-main-screen js-question-number" value="num"> ${number+1} </output>`);
-        });
-        
+        });       
     }
+
     function showResultsScreen() {
         // console.log(`showResultsScreen is:`);
         $(".js-button-restart").toggle();
@@ -147,7 +147,6 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
     function renderOneQuestion(index) {
         let question = questions[index];
     // console.log(`question is ${question}`);
-        // let questionElement = `<h4>${question}</h4>`
         return question;         
     }    
     function renderOneAnswerSet(index) {//generateAnswerElement
@@ -155,15 +154,13 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
         console.log(`answerSet is ${answerSet}`);      
             return answerSet;
         }  
+
     function generateQuestionSet(number) {
-            let answer = renderOneAnswerSet(number);
-        console.log(`answer is ${answer}`);
-        //     let question = renderOneQuestion(number);
-        // console.log(`question is ${question}`)
+        let answer = renderOneAnswerSet(number);
+    // console.log(`answer is ${answer}`);
             let questionsSet = 
             `<h4 role="" class="form js-question-set" id="js-question${number}"> ${questions[number]}</h4>`
-            // questionsSet.join("");
-        console.log(`questionsSet is ${questionsSet}`); 
+    // console.log(`questionsSet is ${questionsSet}`); 
 
             let answersSet = answer.map((answers) =>{
                 return `<input role="" class="radio js-question-set js-button-index0" type="radio" name="options" value="0" checked id="index0">
@@ -173,8 +170,7 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
                 
             })
             answersSet.join("");
-            console.log(`answersSet is ${answersSet}`)
-
+    // console.log(`answersSet is ${answersSet}`)
             let questionSet = `
             ${questionsSet}
             ${answersSet}       
@@ -185,18 +181,15 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
             $(".js-question-set").html(questionSet);
             });
 
-        console.log(`questionSet is ${questionSet}`)
+    //console.log(`questionSet is ${questionSet}`)
             return questionSet;
-        } 
-    function restartQuiz() {
-        
-    };
+        }
    
     function handleNextButton(number) {
-        if(number <= 5) {
+        // checkQuestionSet(number);
         $(".js-button-next").click(function(event) {
             event.preventDefault();
-            
+            if(number <= 5) {
             number++;
             // checkQuestionSet(number)
             questionNumber = `<output role="header" class="info js-question-number" value="num"> Question Number ${number+1} </output>`;
@@ -207,18 +200,35 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
             let questionSet = showNextQuestion(number);
             $(".js-question-set").html(questionSet);              
             // console.log(`number is now ${number}`);   
-            });
         } else {
             $(".js-button-next").click(function(event) {
                 event.preventDefault();
+                // showResultsScreen();
+                number = 0;
+                questionNumber = `<output role="header" class="info js-question-number" value="num"> Question Number ${number} </output>`;
+                $(".js-question-number").html(`${questionNumber}`);
+                // number = 0;
             // $(".js-question-number").html(`${number}`);
             // $(".js-button-restart").toggle();
             $(".js-start-screen").toggle();
             $(".js-main-screen").toggle();
-            number = 0;
-            
+            // number = 0;
             });
+
         }
+
+
+            });
+        
+            // $(".js-button-next").click(function(event) {
+            //     event.preventDefault();
+            // // $(".js-question-number").html(`${number}`);
+            // // $(".js-button-restart").toggle();
+            // $(".js-start-screen").toggle();
+            // $(".js-main-screen").toggle();
+            // number = 0;
+            // });
+        
     }
     function showNextQuestion(questionNumber) {
         let nextQuestion = generateQuestionSet(questionNumber);
@@ -234,13 +244,26 @@ let questionsCorrect = `<output role="header" class="info js-answers-correct">An
     }
     function checkQuestionSet(number) {
         if(number <= 5) {
+            // number++;
+            $(".js-button-next").click(function(event) {
+            event.preventDefault();
+            number++;
             showNextQuestion(number);
+            });
             // number++
         } else {
+            // number = 0;
+            $(".js-button-next").click(function(event) {
+            event.preventDefault();
+            number = 0;
             $("results").toggle();
+            });
         }
-
     }  
+
+    function restartQuiz() {
+        
+    };
 // returnQuestionAtI(0);
 function handleQuiz() {
 
